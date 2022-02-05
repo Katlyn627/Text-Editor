3649b3fda0b2888db76b9d913993cccefabb9795
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-04 19:54:45 -0700

    updated chache

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -5,37 +5,43 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Caching js and css requires workbox-strategies to be installed
+// To actually respond to requests with a cached response, we need to use a strategy called StaleWhileRevalidate
+// This strategy will first check the cache for a response, and if it finds one, it will return it.
+
+// The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-    cacheName: 'page-cache',
-    plugins: [
-        new CacheableResponsePlugin({
-            statuses: [0, 200],
-        }),
-        new ExpirationPlugin({
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-        }),
-    ],
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
 });
 
 warmStrategyCache({
-    urls: ['/index.html', '/'],
-    strategy: pageCache,
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+// Register route for caching images
+// The cache first strategy is often the best choice for images because it saves bandwidth and improves performance.
 registerRoute(
-    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-    new StaleWhileRevalidate({
-        cacheName: 'asset-cache',
-        plugins: [
-            new CacheableResponsePlugin({
-                statuses: [0, 200],
-            }),
-
-        ],
-    })
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
